5afd89b28d1b74e58d0c34d3beae3c3953b298c2
asiya-ahmed-ibrahim <assiyaahmed75@gmail.com>
2026-07-20 08:42:00 +0300

    added folder structure and some starting contents for the page

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Projects from "./pages/Projects/Projects";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  );
+}
+
+export default App;
